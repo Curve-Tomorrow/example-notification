1577ea76c75449ea005d2202033f34452ff3acca
Andre <andremarchezini@gmail.com>
2021-02-15 14:50:11 +0800

    fixed open settings

--- platforms/android/app/src/main/java/cordova/plugin/PowerOptimization/ProtectedApps.java
+++ platforms/android/app/src/main/java/cordova/plugin/PowerOptimization/ProtectedApps.java
@@ -1,48 +1,70 @@
 package cordova.plugin.PowerOptimization;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.util.Log;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 
 import org.apache.cordova.CallbackContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ProtectedApps {
     public static final String TAG = "ProtectedApps";
 
-    // This method checks if can fire the one of a list of intents and fire it
     public static void ProtectedAppCheck(Context context, CallbackContext callbackContext, boolean forceShow) throws JSONException {
-        Log.d(TAG, "ProtectedAppCheck - forceShow: " + forceShow);
-        JSONObject result = new JSONObject();
-        // Get preferences of the configuration
+      List<Intent> POWERMANAGER_INTENTS = Arrays.asList(
+                new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+                new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
+                new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
+                new Intent().setComponent(new ComponentName("com.huawei.systemmanager", Build.VERSION.SDK_INT >= Build.VERSION_CODES.P? "com.huawei.systemmanager.startupmgr.ui.StartupNormalAppListActivity": "com.huawei.systemmanager.appcontrol.activity.StartupAppControlActivity")),
+                new Intent().setComponent(new ComponentName("com.coloros.oppoguardelf", "com.coloros.powermanager.fuelgaue.PowerUsageModelActivity")),
+                new Intent().setComponent(new ComponentName("com.coloros.oppoguardelf", "com.coloros.powermanager.fuelgaue.PowerSaverModeActivity")),
+                new Intent().setComponent(new ComponentName("com.coloros.oppoguardelf", "com.coloros.powermanager.fuelgaue.PowerConsumptionActivity")),
+                new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ? new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.startupapp.StartupAppListActivity")).setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).setData(Uri.parse("package:"+ context.getPackageName())) : null,
+                new Intent().setComponent(new ComponentName("com.oppo.safe", "com.oppo.safe.permission.startup.StartupAppListActivity")),
+                new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity")),
+                new Intent().setComponent(new ComponentName("com.iqoo.secure", "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager")),
+                new Intent().setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
+                new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.entry.FunctionActivity")),
+                new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.autostart.AutoStartActivity")),
+                new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity"))
+                        .setData(android.net.Uri.parse("mobilemanager://function/entry/AutoStart")),
+                new Intent().setComponent(new ComponentName("com.meizu.safe", "com.meizu.safe.security.SHOW_APPSEC")).addCategory(Intent.CATEGORY_DEFAULT).putExtra("packageName", context.getPackageName())
+        );
+
         SharedPreferences settings = context.getSharedPreferences("ProtectedApps", Context.MODE_PRIVATE);
-        final SharedPreferences.Editor editor = settings.edit();
         boolean skipMessage = settings.getBoolean("skipProtectedAppCheck", false);
-        boolean foundCorrectIntent = false;
-        if(!skipMessage || forceShow){
-            Constants cons = new Constants(context);
-            for (Intent intent : cons.getPowermanagerIntents()) {
+
+        if (!skipMessage) {
+            final SharedPreferences.Editor editor = settings.edit();
+            boolean foundCorrectIntent = false;
+            for (Intent intent : POWERMANAGER_INTENTS) {
                 if (isCallable(context, intent)) {
                     foundCorrectIntent = true;
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                     break;
                 }
             }
+            if (!foundCorrectIntent) {
+                editor.putBoolean("skipProtectedAppCheck", true);
+                editor.apply();
+            }
         }
-        
-        editor.putBoolean("skipProtectedAppCheck", true);
-        editor.apply();
 
-        result.put("skip_message", skipMessage);
-        result.put("found_intent", foundCorrectIntent);
-        callbackContext.success(result);
+        callbackContext.success("result");
     }
+
 
     // This method returns if can fire the one of a list of intents and fire it
     public static void HaveProtectedAppIntent(Context context, CallbackContext callbackContext) throws JSONException {
